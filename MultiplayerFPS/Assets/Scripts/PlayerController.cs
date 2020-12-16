a8e893d90162a385bb077b5a0bc95f3dd0b74825
Rshetty02 <70402171+Rshetty02@users.noreply.github.com>
2020-12-17 01:00:39 +0530

    Initial commit

---
+++ MultiplayerFPS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 5f;
+
+    private PlayerMotor motor;
+    public Joystick movement;
+
+    public Joystick cam;
+    public float lookSensitivity = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate movement velocity as a 3d vector
+        float xMov = movement.Horizontal;
+        float zMov = movement.Vertical;
+
+
+
+        Vector3 moveHorizontal = transform.right * xMov;
+
+        Vector3 moveVertical = transform.forward * zMov;
+
+        //final movement vector
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized *speed;
+
+        motor.Move(velocity);
+
+
+        //calculate rotation as a 3d vector. This applies to turning around
+        float yRot = cam.Horizontal;
+           
+        Vector3 rotation = new Vector3(0f, yRot , 0f) * lookSensitivity;
+        
+        motor.Rotate(rotation);
+        
+        //calculate  cam rotation as a 3d vector.
+        float xRot = cam.Vertical;
+           
+        Vector3 camera_rotation = new Vector3(xRot, 0f , 0f) * lookSensitivity;
+        
+        motor.cameraRotate(-camera_rotation);
+
+            }
+}
---
+++ MultiplayerFPS/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camerarotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>();
+
+    }
+
+    //Gets a movement Vector
+    public void Move(Vector3 _velocity){
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation){
+        rotation = _rotation;
+    }
+
+    public void cameraRotate(Vector3 _camerarotation){
+        camerarotation = _camerarotation;
+    }
+
+
+    //Run every Physics iteration
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Performs movement based on velocity variable
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero){
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+    }
+
+    void PerformRotation(){
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if(cam != null){
+            cam.transform.Rotate(camerarotation);            
+        }
+    }
+
+}
+
